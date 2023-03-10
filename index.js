b590b8833a348183f4cb2f97255d1f686ebe39f3
Paulo Gonçalves <paulogoncalves@recife.ifpe.edu.br>
2023-03-10 13:04:50 -0300

    Improved code

--- index.js
+++ index.js
@@ -9,75 +9,53 @@ class Grafico {
         this.cor = ["#FFEBCD", "#0000FF", "#8A2BE2", "#A52A2A", "#DEB887", "#5F9EA0", "#7FFF00", "#D2691E", "#FF7F50", "#6495ED", "#FFF8DC", "#DC143C", "#00FFFF", "#00008B", "#008B8B", "#B8860B", "#B8860B", "#A9A9A9", "#006400", "#BDB76B", "#8B008B", "#556B2F", "#FF8C00"];
     }
     datasetResultadoAbsoluto(labels) {
-        let datasets = new Array();
-        for (let i = 0, max = labels.nomes.length; i < max; i++) {
-            datasets[i] = { label: labels.nomes[i], value: this.votosAbsolutos(labels.numeros[i]), color: this.cor[i], highlight: this.cor[i] };
-        }
-        return datasets;
+        return labels.map((e, i) => ({ label: e.nome, value: e.votos, color: this.cor[i], highlight: this.cor[i] }));
     }
     datasetResultadoPercentual(labels) {
-        let votos = new Array();
-        for (let i = 0, max = labels.nomes.length; i < max; i++) {
-            votos[i] = this.votosAbsolutos(labels.numeros[i]);
-        }
-        let sum = votos.reduce((a, b) => a + b);
-        let datasets = new Array();
-        for (let i = 0, max = labels.nomes.length; i < max; i++) {
-            let voto = votos[i] / sum;
-            datasets[i] = { label: labels.nomes[i], value: voto, color: this.cor[i], highlight: this.cor[i] };
-        }
-        return datasets;
+        return labels.map((e, i) => ({ label: e.nome, value: e.perc, color: this.cor[i], highlight: this.cor[i] }));
     }
     dadosBar() {
-        let cand = this.xml.getElementsByTagName("candidatos")[0].getElementsByTagName("candidato");
-        let vetor1 = new Array();
-        let vetor2 = new Array();
-        for (let i = 0, max = cand.length; i < max; i++) {
-            vetor1[i] = cand[i].getAttribute("nome");
-            vetor2[i] = cand[i].getAttribute("n");
-        }
-        return { nomes: vetor1, numeros: vetor2 };
+        let temp = Array.from(this.xml.querySelectorAll("candidato")).map(e => ({ nome: e.getAttribute("nome"), numero: e.getAttribute("n"), votos: this.votosAbsolutos(e.getAttribute("n")) }));
+        let sum = temp.reduce((a, b) => a + b.votos, 0);
+        temp.forEach(e => e.perc = e.votos / sum);
+        return temp;
     }
     gerarGraficoPizzaResultadoAbsoluto() {
-        if (this.xhr.readyState === 4) {
-            this.xml = this.xhr.responseXML;
-            let labels = this.dadosBar();
-            let dados = this.datasetResultadoAbsoluto(labels);
-            Chart.defaults.global.tooltipTemplate = "<%if (label){%><%=label%>: <%}%><%= new Intl.NumberFormat('pt-br').format(value) %>";
-            this.setPieChart(dados);
-            document.getElementById("mapa").style.display = "none";
-            document.getElementById("chart").style.display = "block";
-        }
+        this.xml = this.xhr.responseXML;
+        let labels = this.dadosBar();
+        let dados = this.datasetResultadoAbsoluto(labels);
+        Chart.defaults.global.tooltipTemplate = "<%if (label){%><%=label%>: <%}%><%= new Intl.NumberFormat('pt-br').format(value) %>";
+        this.setPieChart(dados);
+        document.getElementById("mapa").style.display = "none";
+        document.getElementById("chart").style.display = "block";
     }
     setPieChart(dados) {
-        if (this.myChart !== null) {
+        if (this.myChart) {
             this.myChart.destroy();
         }
         this.myChart = this.chart.Pie(dados);
     }
     gerarGraficoPizzaResultadoPercentual() {
-        if (this.xhr.readyState === 4) {
-            this.xml = this.xhr.responseXML;
-            let labels = this.dadosBar();
-            let dados = this.datasetResultadoPercentual(labels);
-            Chart.defaults.global.tooltipTemplate = "<%if (label){%><%=label%>: <%}%><%= new Intl.NumberFormat('pt-br', {style: 'percent', minimumFractionDigits: 2, maximumFractionDigits: 2}).format(value) %>";
-            this.setPieChart(dados);
-            document.getElementById("mapa").style.display = "none";
-            document.getElementById("chart").style.display = "block";
-        }
+        this.xml = this.xhr.responseXML;
+        let labels = this.dadosBar();
+        let dados = this.datasetResultadoPercentual(labels);
+        Chart.defaults.global.tooltipTemplate = "<%if (label){%><%=label%>: <%}%><%= new Intl.NumberFormat('pt-br', {style: 'percent', minimumFractionDigits: 2, maximumFractionDigits: 2}).format(value) %>";
+        this.setPieChart(dados);
+        document.getElementById("mapa").style.display = "none";
+        document.getElementById("chart").style.display = "block";
     }
     resultadoAbsoluto() {
-        this.xhr.open("get", "Eleicao" + this.ano + "-Presidente.xml", true);
-        this.xhr.onreadystatechange = this.gerarGraficoPizzaResultadoAbsoluto.bind(this);
+        this.xhr.open("get", `Eleicao${this.ano}-Presidente.xml`);
+        this.xhr.onload = this.gerarGraficoPizzaResultadoAbsoluto.bind(this);
         this.xhr.send(null);
     }
-    resultadoPercentual = function () {
-        this.xhr.open("get", "Eleicao" + this.ano + "-Presidente.xml", true);
-        this.xhr.onreadystatechange = this.gerarGraficoPizzaResultadoPercentual.bind(this);
+    resultadoPercentual() {
+        this.xhr.open("get", `Eleicao${this.ano}-Presidente.xml`);
+        this.xhr.onload = this.gerarGraficoPizzaResultadoPercentual.bind(this);
         this.xhr.send(null);
     }
     votosAbsolutos(n) {
-        let iterator = this.xml.evaluate("//turno[@n=" + this.turno + "]//votos[@n=" + n + "]", this.xml, null, XPathResult.UNORDERED_NODE_ITERATOR_TYPE, null);
+        let iterator = this.xml.evaluate(`//turno[@n=${this.turno}]//votos[@n=${n}]`, this.xml, null, XPathResult.UNORDERED_NODE_ITERATOR_TYPE, null);
         let sum = 0;
         try {
             var thisNode = iterator.iterateNext();
